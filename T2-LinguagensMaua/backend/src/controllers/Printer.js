a05d23511e7b20fdf28369fe79c5d20afe6571a9
Morfogon <44737812+Morfogon@users.noreply.github.com>
2021-11-03 17:33:08 -0300

    Delete T2-LinguagensMaua directory

--- T2-LinguagensMaua/backend/src/controllers/Printer.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../database/connections');
-
-module.exports = {
-    async create(request,response){
-        const { name,email} = request.body;
-        
-        return response.json({ name,email});
-    }
-};
